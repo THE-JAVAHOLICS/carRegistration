24a0e9b16d07ebac707c7e037ee0fd4298cf8fa0
omer-bt <omerbt1@hotmail.com>
2021-04-13 23:39:03 +0400

    first_try

--- carRegistrationMaven/src/main/java/org/example/App.java
+++ carRegistrationMaven/src/main/java/org/example/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        System.out.println("Hello me");
+        System.out.println("Hello there");
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
